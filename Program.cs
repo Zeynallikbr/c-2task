cc9cf6eb0fab5592d3f047bfdc3f3b8080ef9b28
ekber <zeynallikbr2@gmail.com>
2024-01-21 19:35:22 +0400

    c#2

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Week2.app2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heftenin gununu girin");
+            int dayNumber = int.Parse(Console.ReadLine());
+            if (dayNumber == 1)
+            {
+                Console.WriteLine("univeristet");
+            }
+            else if(dayNumber ==2)
+            {
+                Console.WriteLine("universitet + code academy task");
+            }
+            else if (dayNumber == 3)
+            {
+                Console.WriteLine("universitet + footbal");
+            }
+            else if (dayNumber == 4)
+            {
+                Console.WriteLine("universitet + film");
+            }
+            else if (dayNumber == 5)
+            {
+                Console.WriteLine("universitet + ingilis dili ");
+            }
+            else if (dayNumber == 6)
+            {
+                Console.WriteLine("universitet + codeacademy");
+            }
+            else if (dayNumber == 7)
+            {
+                Console.WriteLine("universitet + codeacademy");
+            }
+        }
+    }
+}
